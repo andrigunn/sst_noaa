0a6ae4eb67ba6130ed035d1ca651d1f8cdd8d615
Andri Gunnarsson <andri140183@gmail.com>
2024-08-28 10:06:00 +0000

    Update sst_noaa_daily_plotting.m

--- sst_noaa_daily_plotting.m
+++ sst_noaa_daily_plotting.m
@@ -3,7 +3,9 @@ load('/projects/sst_noaa/geo_noaa_hiRes_sst.mat')
 addpath /git/timeseriestools
 addpath /git/cdt
 %%
-[overlay_table,overlay_table_cumsum,tbl] = timetable2overlaystructure(SST)
+baseline_period = [datetime(1990,01,01),datetime(2020,12,31)];
+
+[overlay_table,overlay_table_cumsum,tbl] = timetable2overlaystructure(SST,baseline_period)
 %%
 set(0,'defaultfigurepaperunits','centimeters');
 set(0,'DefaultAxesFontSize',16)
